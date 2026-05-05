8d578f85a1a9e5ae7f1b3535b29fc0603e305a58
NazarStyranec3 <nazarsturanec@gmail.com>
2026-05-05 21:18:05 +0100

    start

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about_me/', views.about_me, name='about_me'),
-    path('robots.txt', views.robots_txt),  # 👈 ДОДАЙ ЦЕ
+    path('robots.txt', views.robots_txt, name='robots_txt'),
+    path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
 
-
 from django.http import HttpResponse
+
 
 def robots_txt(request):
     return HttpResponse(
-        "User-agent: *\nDisallow: /",
+        "User-agent: *\nDisallow: /\n",
         content_type="text/plain"
     )
-# Create your views here.
-# Create your views here.
+
+
+def sitemap_xml(request):
+    return HttpResponse(
+        """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://nstyranets.com/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://nstyranets.com/about_me/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+</urlset>
+""",
+        content_type="application/xml"
+    )
+
+
 def home(request):
     return render(request, 'main/home.html')
+
 
 def about_me(request):
     return render(request, 'main/about_me.html')
